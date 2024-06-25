bdaafdd57104809b407ff96dc331825ff5f81548
Andrey Pivovarov <andrey.pivovarov@customertimes.com>
2024-06-25 14:05:55 +0300

    created CarService interface CarService was injected to CarController

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,32 +1,24 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import web.model.Car;
-import web.service.CarServiceImpl;
-
-import java.util.ArrayList;
-import java.util.List;
+import web.service.CarService;
 
 @Controller
 @RequestMapping("/cars")
 public class CarController {
 
-    List<Car> carList = new ArrayList<>(){{
-        add(new Car("BMW", "X", 5));
-        add(new Car("BMW", "X", 3));
-        add(new Car("Opel", "Astra", 1));
-        add(new Car("Audi", "A", 8));
-        add(new Car("Audi", "A", 7));
-    }};
+    @Autowired
+    private CarService carService;
 
     @GetMapping
     public String getCars(ModelMap modelMap,
                           @RequestParam(required = false, name = "count", defaultValue = "-1") int count) {
-        modelMap.addAttribute("cars", CarServiceImpl.getCars(carList, count));
+        modelMap.addAttribute("cars", carService.getCars(count));
         return "cars";
     }
 }
--- src/main/java/web/service/CarServiceImpl.java
+++ src/main/java/web/service/CarServiceImpl.java
@@ -1,12 +1,23 @@
 package web.service;
 
+import org.springframework.stereotype.Service;
 import web.model.Car;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class CarServiceImpl {
+@Service
+public class CarServiceImpl implements CarService {
 
-    public static List<Car> getCars(List<Car> carList, int count) {
+    List<Car> carList = new ArrayList<>(){{
+        add(new Car("BMW", "X", 5));
+        add(new Car("BMW", "X", 3));
+        add(new Car("Opel", "Astra", 1));
+        add(new Car("Audi", "A", 8));
+        add(new Car("Audi", "A", 7));
+    }};
+
+    public List<Car> getCars(int count) {
         if (count >= 0) {
             return carList.subList(0, count);
         } else {
